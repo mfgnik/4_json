7b31345dd96727fafc2d52549bbff3772c3287a6
mfgnik <31517000+mfgnik@users.noreply.github.com>
2018-02-27 13:39:13 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,16 @@
 import json
+import sys
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r', encoding='utf-8') as json_file:
+        return json.load(json_file)
 
 
-def pretty_print_json(data):
-    pass
+def pretty_print_json(json_file_content):
+    return json.dumps(json_file_content, indent=3, ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    pass
+    file_path = sys.argv[1]
+    print(pretty_print_json(load_data(file_path)))
